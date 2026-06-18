fc01373b0fa0352f04836d400afbed2841ba33cd
darshanmangukiya.it23d1@scet.ac.in <darshanmangukiya95@gmail.com>
2026-06-18 12:06:50 +0530

    Again changed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -8,7 +8,7 @@ const askPlantQuestion = async (req, res) => {
         const { plantName, question } = req.body;
 
         const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash"
+            model: "gemini-2.5-flash"
         });
 
         const prompt = `You are a plant expert. 
